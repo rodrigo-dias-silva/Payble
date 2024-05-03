d6eec32604ed8e1971a75f66b2766a9111e4b776
Rodrigo Linux <rodrigo-cleiton@hotmail.com>
2024-05-03 01:18:33 -0300

    feat: create login page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import Header from "@/components/Header";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
 
 export default function Home() {
   return (
-    <h1>Payble</h1>
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <Header />
+        <form
+          className="flex flex-col justify-center items-center bg-gray-100 p-10 -mt-6 md:w-96 md:h-96 space-y-4 rounded-md"
+        >
+          <span className="text-xl font-light mb-4">
+            Faça seu login
+          </span>
+          <Input placeholder="Digite seu usuario" />
+          <Input placeholder="Digite sua senha" />
+          <Button />
+
+        </form>
+      </div>
+    </>
+
   );
 }
